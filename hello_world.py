0b8ee98057a63d80774b8155c61521956dbed5bc
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-06-01 12:07:00 +0200

    implemented argparse for hello sunshine script

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
-print("hello sunshine")
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--sunshine", action="store_true",
+                    dest = "sunshine", default=False)
+arguments = parser.parse_args()
+
+if arguments.sunshine:
+    print("hello sunshine")
