5e5299ce5944d7a88c3a1c53d5292381c800171a
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-06-01 21:52:08 +0200

    used output_folder as argparse parameter

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from obtain_sequences import use_amplicon
 parser = argparse.ArgumentParser()
 parser.add_argument("-g", "--genus_list", action="store",
                     dest="genus_list", required=True)
-arguments = parser.parse_args()
+parser.add_argument("-o", "--output_folder", action="store",
+                    dest="output_folder", required=True)
+args = parser.parse_args()
 
-taxids = use_amplicon.get_taxid(input_file=arguments.genus_list)
-use_amplicon.get_protein_sequences(taxids, output_folder="/tmp/ztr/")
+taxids = use_amplicon.get_taxid(input_file=args.genus_list)
+use_amplicon.get_protein_sequences(taxids, args.output_folder)
