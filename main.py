9219cb788b5a899ed0e5dc5ac26de4c2d0026ba7
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-06-01 16:52:37 +0200

    added argparse to main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 
+import argparse
 from obtain_sequences import use_amplicon
 
-res = use_amplicon.get_taxid(input_file="data/genus_list.txt")
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--genus_list", action="store",
+                    dest="genus_list", required=True)
+arguments = parser.parse_args()
+
+res = use_amplicon.get_taxid(input_file=arguments.genus_list)
 print(res)
