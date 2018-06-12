e2cc0edf24fa1ad8a871aecd6508fcb1e104a7e8
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-06-12 11:51:11 +0200

    implemented assembled metagenome data (closes #1)

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import argparse
+from obtain_sequences import use_amplicon, use_assembled
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--mode", choices=["amplicon", "assembled", "unassembled"], dest="mode",
+                    required=True, help="mode for analysis (amplicon, assembled, unassembled)")
+args = parser.parse_known_args()[0]
+if args.mode == "amplicon":
+    parser.add_argument("-b", "--remove_backup", action="store_false", dest="remove_backup",
+                        required=False, help="remove backup files")
+    parser.add_argument("-g", "--genus_list", action="store", dest="genus_list", required=True,
+                        help="list of genera used for amplicon analysis")
+    parser.add_argument("-n", "--names_dmp", action="store", dest="names_dmp", default=None,
+                        required=False, help="location of names.dmp")
+    parser.add_argument("-r", "--reviewed", action="store_true", dest="reviewed", required=False,
+                        help="use all unreviewed TrEMBL hits (default) or only reviewed SwissProt")
+    parser.add_argument("-t", "--taxonomy", action="store_false", dest="taxonomy", required=False,
+                        help="add taxonomic lineage to fasta header")
+elif args.mode == "assembled":
+    parser.add_argument("-p", "--assembled", action="store", dest="metagenome_assembled",
+                        required=True, help="protein file of assembled metagenome")
+parser.add_argument("-o", "--output_folder", action="store", dest="output_folder", required=True,
+                    help="output folder")
+args = parser.parse_args()
+
+# run amplicon analysis
+if args.mode == "amplicon":
+    abspath_names_dmp = use_amplicon.get_names_dmp(names_dmp=args.names_dmp)
+    tax_dict = use_amplicon.create_tax_dict(abspath_names_dmp=abspath_names_dmp)
+    taxids = use_amplicon.get_taxid(input_file=args.genus_list)
+    use_amplicon.get_protein_sequences(tax_list=taxids, output_folder=args.output_folder,
+                                       ncbi_tax_dict=tax_dict, reviewed=args.reviewed,
+                                       add_taxonomy=args.taxonomy,
+                                       remove_backup=args.remove_backup)
+elif args.mode == "assembled":
+    use_assembled.is_fasta(args.metagenome_assembled)
+    print("successfully loaded assembled metagenome file")
+
+# run assembled metagenome analysis
+
+# run unassembled metagenome analysis
+
+# remove duplicates
+
+# hash headers
---
+++ obtain_sequences/use_assembled.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+Check the input assembled fasta file for valid protein fasta.
+
+The passed file will be validated if it is a correct protein fasta file.
+"""
+
+import sys
+from Bio import Alphabet
+from Bio import SeqIO
+
+
+def is_fasta(input_file):
+    """
+    Get taxonomic lineage on taxid for desired ranks.
+
+    The function get_desired_ranks uses a taxid as input and returns a dict of ranks (superkingdom,
+    phylum, class, order, family, genus, species) as keys and corresponding taxIDs as values.
+
+    Parameters
+    ----------
+      taxid: TaxID (-1 represents unclassified)
+
+    Returns
+    -------
+      ranks2lineage: dict with ranks as keys and taxIDs as values
+
+    """
+    with open(input_file, "r") as handle:
+        fasta = SeqIO.parse(handle, "fasta", Alphabet.generic_protein)
+        if any(fasta) is False:
+            print(input_file + " is not a valid protein fasta file. Exit code: 3. Exiting ... ")
+            sys.exit(3)
+        else:
+            return None
