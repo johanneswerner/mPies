d28c4491f626629046b084565f69e190436290b2
Johannes Werner <johannes.werner@io-warnemuende.de>
2018-06-13 10:28:57 +0200

    implemented main function (fixes #6)

--- main.py
+++ main.py
@@ -5,51 +5,53 @@ import os
 import sys
 from pies import use_amplicon
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-m", "--mode", choices=["amplicon", "assembled", "unassembled"], dest="mode",
-                    required=True, help="mode for analysis (amplicon, assembled, unassembled)")
-args = parser.parse_known_args()[0]
-if args.mode == "amplicon":
-    parser.add_argument("-g", "--genus_list", action="store", dest="genus_list", required=True,
-                        help="list of genera used for amplicon analysis")
-    parser.add_argument("-n", "--names_dmp", action="store", dest="names_dmp", default=None,
-                        required=False, help="location of names.dmp")
-    parser.add_argument("-r", "--reviewed", action="store_true", dest="reviewed", required=False,
-                        help="use all unreviewed TrEMBL hits (default) or only reviewed SwissProt")
-    parser.add_argument("-t", "--taxonomy", action="store_false", dest="taxonomy", required=False,
-                        help="add taxonomic lineage to fasta header")
-parser.add_argument("-o", "--output_folder", action="store", dest="output_folder", required=True,
-                    help="output folder")
-parser.add_argument("-b", "--remove_backup", action="store_false", dest="remove_backup",
-                    required=False, help="remove backup files")
-args = parser.parse_args()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-m", "--mode", choices=["amplicon", "assembled", "unassembled"],
+                        dest="mode", required=True,
+                        help="mode for analysis (amplicon, assembled, unassembled)")
+    args = parser.parse_known_args()[0]
+    if args.mode == "amplicon":
+        parser.add_argument("-g", "--genus_list", action="store", dest="genus_list", required=True,
+                            help="list of genera used for amplicon analysis")
+        parser.add_argument("-n", "--names_dmp", action="store", dest="names_dmp", default=None,
+                            required=False, help="location of names.dmp")
+        parser.add_argument("-r", "--reviewed", action="store_true", dest="reviewed",
+                            required=False,
+                            help="use unreviewed TrEMBL hits (default) or only reviewed SwissProt")
+        parser.add_argument("-t", "--taxonomy", action="store_false", dest="taxonomy",
+                            required=False, help="add taxonomic lineage to fasta header")
+    elif args.mode == "assembled":
+        parser.add_argument("-p", "--assembled", action="store", dest="metagenome_assembled",
+                            required=True, help="protein file of assembled metagenome")
+    parser.add_argument("-o", "--output_folder", action="store", dest="output_folder",
+                        required=True, help="output folder")
+    parser.add_argument("-b", "--remove_backup", action="store_false", dest="remove_backup",
+                        required=False, help="remove backup files")
+    args = parser.parse_args()
+
+    if os.path.exists(args.output_folder):
+        print("Error. Output folder already exists. Exit code: 1. Exiting ...")
+        sys.exit(1)
+    else:
+        os.makedirs(args.output_folder)
+    # run amplicon analysis
+    if args.mode == "amplicon":
+        abspath_names_dmp = use_amplicon.get_names_dmp(names_dmp=args.names_dmp)
+        tax_dict = use_amplicon.create_tax_dict(abspath_names_dmp=abspath_names_dmp)
+        taxids = use_amplicon.get_taxid(input_file=args.genus_list)
+        use_amplicon.get_protein_sequences(tax_list=taxids, output_folder=args.output_folder,
+                                           ncbi_tax_dict=tax_dict, reviewed=args.reviewed,
+                                           add_taxonomy=args.taxonomy,
+                                           remove_backup=args.remove_backup)
+        fasta_file = use_amplicon.combine_fasta_files(fasta_folder=args.output_folder,
+                                                      remove_single_files=True)
+
+    print(fasta_file)
+
+    # hash headers
 
 
-if os.path.exists(args.output_folder):
-    print("Error. Output folder already exists. Exit code: 1. Exiting ...")
-    sys.exit(1)
-else:
-    os.makedirs(args.output_folder)
-# run amplicon analysis
-if args.mode == "amplicon":
-    abspath_names_dmp = use_amplicon.get_names_dmp(names_dmp=args.names_dmp)
-    tax_dict = use_amplicon.create_tax_dict(abspath_names_dmp=abspath_names_dmp)
-    taxids = use_amplicon.get_taxid(input_file=args.genus_list)
-    use_amplicon.get_protein_sequences(tax_list=taxids, output_folder=args.output_folder,
-                                       ncbi_tax_dict=tax_dict, reviewed=args.reviewed,
-                                       add_taxonomy=args.taxonomy,
-                                       remove_backup=args.remove_backup)
-    fasta_file = use_amplicon.combine_fasta_files(fasta_folder=args.output_folder,
-
-# run assembled metagenome analysis
-
-print(fasta_file)
-
-# run unassembled metagenome analysis
-
-# remove duplicates
-
-# hash headers
-
-print("successfully finished script")
-sys.exit(0)
+if __name__ == "main":
+    main()
