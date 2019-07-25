258f5396c4be1f429788da59ce0bf96a4294a577
Johannes Werner <johannes.werner@io-warnemuende.de>
2019-07-25 15:43:18 +0200

    added score_cutoff parameter for parse_table function in parse_taxonomy module

--- mptk/parse_taxonomy.py
+++ mptk/parse_taxonomy.py
@@ -12,7 +12,7 @@ import pandas as pd
 logger = logging.getLogger("mptk.parse_taxonomy")
 
 
-def parse_table(input_file, output_file):
+def parse_table(input_file, output_file, score_cutoff=0):
     """
     Read the MEGAN results table and creates a tab-separated output table with the taxonomy results.
 
@@ -24,6 +24,7 @@ def parse_table(input_file, output_file):
       input_file: MEGAN taxonomy table
       output_file: path where to create output, a tab-separated table with accession number, taxonomic ranks, and
                    score values
+      score_cutoff: quality cutoff (percentage of the reads) to report a taxonomic rank
 
     Returns
     -------
@@ -34,6 +35,16 @@ def parse_table(input_file, output_file):
                     "f_name", "f_score", "g_name", "g_score", "s_name", "s_score", "__blank"]
     df = pd.read_csv(input_file, sep=";", engine="python", header=None, names=column_names, usecols=list(range(17)))
     df.drop(df.columns[[1, -1]], axis=1, inplace=True)
+
+    if score_cutoff != 0:
+        df.loc[df["d_score"] < score_cutoff, "d_name"] = "d__belowScoreCutoff"
+        df.loc[df["p_score"] < score_cutoff, "p_name"] = "p__belowScoreCutoff"
+        df.loc[df["c_score"] < score_cutoff, "c_name"] = "c__belowScoreCutoff"
+        df.loc[df["o_score"] < score_cutoff, "o_name"] = "o__belowScoreCutoff"
+        df.loc[df["f_score"] < score_cutoff, "f_name"] = "f__belowScoreCutoff"
+        df.loc[df["g_score"] < score_cutoff, "g_name"] = "g__belowScoreCutoff"
+        df.loc[df["s_score"] < score_cutoff, "s_name"] = "s__belowScoreCutoff"
+
     df.rename(columns={"id": "protein_group"}, inplace=True)
     df = df.groupby("protein_group").head(1)
 
